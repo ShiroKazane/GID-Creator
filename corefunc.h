2092ea9230a0459e734ef03bcd45f4a6152d4b6e
Shiro <49191595+ShiroKazane@users.noreply.github.com>
2021-12-22 17:28:25 +0700

    Update corefunc.h

--- corefunc.h
+++ corefunc.h
@@ -314,7 +314,7 @@ public:
 	    				string growid = explode("`` created.  Write it and your password", explode("add_textbox|A `wGrowID`` with the log on of `w", ctx)[1])[0];
 	    				string passwr = ""+ Password_acc;
 	    				cout << "Account succesfuly created! GrowID: " << growid << ",Password: " << Password_acc << endl;
-	    				appendAcc("User: " + growid + ",Password: " + Password_acc);
+	    				appendAcc(growid + "|" + Password_acc);
 	    				enet_peer_disconnect_later(peer, 0);
 	    			}
 	    			//cout << ctx << endl;
